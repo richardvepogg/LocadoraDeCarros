4ba611530c33ad3155e0e7de56b3ff865def4b1c
Richard Vepo <richardvepogg@gmail.com>
2022-02-05 21:49:54 -0300

    migration foram realizadas com sucesso

---
+++ Presentaçao/Dados/Migrations/20220206004542_adiciona-telefones.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dados.Migrations
+{
+    public partial class adicionatelefones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Cliente");
+
+            migrationBuilder.CreateTable(
+                name: "TelefoneDataModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClienteID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelefoneDataModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TelefoneDataModel_Cliente_ClienteID",
+                        column: x => x.ClienteID,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelefoneDataModel_ClienteID",
+                table: "TelefoneDataModel",
+                column: "ClienteID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelefoneDataModel");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Presentaçao/Dados/Models/ClienteDataModel.cs
+++ Presentaçao/Dados/Models/ClienteDataModel.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dados.Models
 {
     public class ClienteDataModel
     {
-
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Endereco { get; set; }
-        public string Telefone { get; set; }
         public string Email { get; set; }
         public string CarteiraDeMotorista { get; set; }
         public string CartaoDeCredito { get; set; }
-
+        public virtual ICollection<TelefoneDataModel> Telefones { get; set; }
     }
 }
